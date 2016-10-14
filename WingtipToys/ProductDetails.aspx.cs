97eac8adf098d0f948d47511cda9e76574cb3085
Binh Tran <binhtran@kms-technology.com>
2016-10-15 01:18:54 +0700

    Complete Display Data Item and Detail

--- WingtipToys/ProductDetails.aspx.cs
+++ WingtipToys/ProductDetails.aspx.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.ModelBinding;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using WingtipToys.Models;
 
 namespace WingtipToys
 {
@@ -12,6 +14,21 @@ namespace WingtipToys
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        public IQueryable<Product> GetProduct([QueryString("productID")] int? productId)
+        {
+            var dbContext = new WingtipToys.Models.ProductContext();
+            IQueryable<Product> query = dbContext.Products;
+            if (productId.HasValue && productId > 0)
+            {
+                query = query.Where(p => p.ProductID == productId);
+            }
+            else
+            {
+                query = null;
+            }
+            return query;
         }
     }
 }
